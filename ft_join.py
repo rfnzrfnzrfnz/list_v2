361b4710136d1c5347d95a4d1d8a2896fe162a17
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-11-12 12:09:51 +0300

    Update ft_join.py

--- ft_join.py
+++ ft_join.py
@@ -1,6 +1,6 @@
 def ft_join(lst, sep=" "):
     res_str = ''
     for item in lst[:-1]:
-        res_str += f"{item}" + sep
-    res_str += f"{lst[-1]}"
+        res_str += str(item) + sep
+    res_str += str(lst[-1])
     return res_str
