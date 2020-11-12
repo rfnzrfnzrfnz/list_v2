f78fe9157f265141fd408adbd4800ad6723dd6d6
катя <45289493+rfnzrfnzrfnz@users.noreply.github.com>
2020-11-12 12:35:41 +0300

    Update ft_join.py

--- ft_join.py
+++ ft_join.py
@@ -1,15 +1,15 @@
 def ft_len(st):
-    kol = 0
+    l = 0
     for i in st:
-        kol += 1
-    return kol
+        l += 1
+    return l
 
 
-def ft_join(lst, sep=" "):
+def ft_join(lst, se=" "):
     s = ''
     for i in range(ft_len(lst)):
         if i == ft_len(lst) - 1:
             s += lst[i]
         else:
-            s += lst[i] + sep
+            s += lst[i] + se
     return s
